7c9c0f0bbefe129921244f57dab611a7a8e3275a
Lockowom <metalfilt@gmail.com>
2026-02-12 18:58:33 -0300

    agaIN

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vtrtyzbgpsvqwbfoudaf.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || 'sb_publishable_E98rNjBu7rG9mLjgI68sAw_Wr2EIp-f';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
